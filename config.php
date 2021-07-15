54e0f778a91d0a852e774b671d00047d91961f3e
mckenziev <juanev1234@gmail.com>
2021-07-15 15:53:34 -0600

    Commit inicial

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'clinica';
+    try {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	exit('No se pudo conectar a la Base!');
+    }
+}

cf15382f91d7c2e06449959bcca1b3dc04f199f1
mckenziev <juanev1234@gmail.com>
2021-07-15 16:01:06 -0600

    Commit 15/07/2021:16:01

--- config.php
+++ config.php
@@ -10,3 +10,23 @@ function pdo_connect_mysql() {
     	exit('No se pudo conectar a la Base!');
     }
 }
+function encabezado_pagina($titulo) {
+    echo <<<EOT
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    EOT;
+}
+function pie_pagina() {
+    echo <<<EOT
+        </body>
+    </html>
+    EOT;
+    }
+?>
